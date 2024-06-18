f2d74bdc8fb7d67124963cd4c834f0fea72af051
hamatiii <158294123+hamatiii@users.noreply.github.com>
2024-06-18 21:42:55 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <time.h>
 
 using namespace std;
 
-ifstream ifs("qual_random_10.in"); 
+ifstream ifs("qual_random_01.in"); 
 ofstream ofs("result.in", ios::app);
 
 
 int main() {
-
+    clock_t start = clock();
     //ダイクストラ用
     vector<pair<int,int>> G[40009];
 
@@ -27,8 +28,8 @@ int main() {
         for (int j = 0; j < ((i % 2 == 0) ? N - 1 : N); ++j) {
             ifs >> map[i][j];
             if(i%2 == 0){
-                G[j+(i/2)*N].push_back(make_pair(j+(i/2)*N+1,map[i][j]));
-                G[j+(i/2)*N+1].push_back(make_pair(j+(i/2)*N,map[i][j]));
+                G[j+(i/2)*N].push_back(make_pair(j+((i/2)*N)+1,map[i][j]));
+                G[j+((i/2)*N)+1].push_back(make_pair(j+(i/2)*N,map[i][j]));
             }
             if(i%2 == 1){
                 G[j+((i+1)/2)*N].push_back(make_pair(j+(((i+1)/2)-1)*N,map[i][j]));
@@ -92,31 +93,47 @@ int main() {
 
     //ダイクストラ開始
     for(int i = 0;i < Q;i++){
-        int base = Qlist[i][0]+(Qlist[i][1]*N);
-        int distination = Qlist[i][2]+(Qlist[i][3]*N);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pri;
-        int cur[40000] = {200000000};
-        bool kakutei[40000] = {false};
-        cur[base] = 0;
-        pri.push(make_pair(cur[base],0));
+        int X[2] = {Qlist[i][1], Qlist[i][0]};
+        int Y[2] = {Qlist[i][3], Qlist[i][2]};
+        int basenumber;
+        if(X[0] > Y[0]) swap(X, Y);
+        if(X[1] < Y[1]) basenumber = ruiseki[Y[0]+1][Y[1]+1] + ruiseki[X[0]][X[1]] - ruiseki[Y[0]][X[1]] - ruiseki[X[0]][Y[1]];
+        else basenumber = ruiseki[Y[0]+1][X[1]+1] + ruiseki[X[0]][Y[1]] - ruiseki[X[0]][X[1]] - ruiseki[Y[0]][Y[1]];
 
+        //int base = Qlist[i][0]+(Qlist[i][1]*N);
+        //int distination = Qlist[i][2]+(Qlist[i][3]*N);
+        int base = X[1]+(X[0]*N);
+        int distination = Y[1]+(Y[0]*N);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pri;
+        long long cur[40000];
+        for(int j = 0;j < 40000;j++)cur[j] = 2000000000000000;
+        bool kakutei[40000];
+        for(int j = 0;j < 40000;j++) kakutei[j] = false;
+        cur[base] = 0;
+        pri.push(make_pair(cur[base],base));
+        //cout << base << " " << distination << endl;
         while(!pri.empty()){
             int pos = pri.top().second; pri.pop();
-
             if(kakutei[pos] == true) continue;
-
             kakutei[pos] = true;
             for(int j = 0;j < G[pos].size();j++){
                 int nex = G[pos][j].first;
                 int cost = G[pos][j].second;
+                //cout << nex << " " << cost << endl;
                 if(cur[nex] > cur[pos] + cost){
                     cur[nex] = cur[pos] + cost;
                     pri.push(make_pair(cur[nex], nex));
                 }
+                else{
+                    //cout << cur[pos] + cost << " " << "asd" << endl;
+                }
             }
         }
+        
+        cout << i << endl;
         ofs << cur[distination] << endl;
-        cout << distination << endl;
+        //cout << distination << endl;
     }
-
+    clock_t end = clock();
+    cout << end-start;
 }
